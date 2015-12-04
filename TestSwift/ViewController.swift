09b1dc2e59846e06601eb75f2f283527cc8495d8
Nicolas Miranda <Nicolas@nicolass-macbook-pro-2.local>
2015-12-03 19:37:24 -0500

    Initial Commit

---
+++ TestSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestSwift
+//
+//  Created by Nicolas Miranda on 3/12/15.
+//  Copyright © 2015 RaulQuispe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
